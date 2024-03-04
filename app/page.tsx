a5ddaa49d220697a8be813f0596215eb42726c4e
LORENA DA SILVA SANTOS <lorena.santos81>
2024-03-04 09:49:16 -0300

    aula css

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
 export default function Page() {
-  return <h1> Hello, Next.js! </h1>
+  return(
+    <>
+      <h1> Hello, Next.js! </h1>
+      <h2> Titulo 2 </h2>
+      <h3> Titulo 3</h3>
+    </>
+  )
 }
