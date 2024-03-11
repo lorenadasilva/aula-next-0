2cf144fce364ba3b4775d790e426af7ca1dacb05
LORENA DA SILVA SANTOS <lorena.santos81>
2024-03-11 09:47:49 -0300

    aula1-11

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import Link from 'next/link';
+
 export default function Page() {
   return(
     <>
       <h1> Hello, Next.js! </h1>
       <h2> Titulo 2 </h2>
       <h3> Titulo 3</h3>
+      Qualquer coisa
+
+      <Link href={'/sobre'}> Sobre </Link>
+      <Link href={'/cadusuario'}> Cadastro de usuario </Link>
     </>
   )
 }
